dc28f5aec8926a36ec6ee3b0a8bcb687675e76b4
Daniel Kopecky <kopecky.d@gmail.com>
2020-01-20 15:48:39 +0100

    New Errors

---
+++ invenio_oarepo_oai_pmh_harvester/exceptions.py
@@ -0,0 +1,14 @@
+class EndPointNotFoundError(Exception):
+    pass
+
+
+class PrefixNotFoundError(Exception):
+    pass
+
+
+class ProviderNotFoundError(Exception):
+    pass
+
+
+class RuleRequiredError(Exception):
+    pass
