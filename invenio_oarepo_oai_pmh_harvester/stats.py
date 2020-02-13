787505700d647acfd1471596167ee1bd9531d7e5
Daniel Kopecky <kopecky.d@gmail.com>
2020-02-13 15:06:09 +0100

    Added logger and json for save final statistic data

--- invenio_oarepo_oai_pmh_harvester/stats.py
+++ invenio_oarepo_oai_pmh_harvester/stats.py
@@ -1,10 +1,36 @@
+import json
+import logging
+import os
 from collections import deque
 
 import jmespath
 from sickle import Sickle
 
+from invenio_oarepo_oai_pmh_harvester.json import SetEncoder
 from invenio_oarepo_oai_pmh_harvester.models import OAIProvider
 from invenio_oarepo_oai_pmh_harvester.utils import sanitize_address, add_node, update_node
+
+log_dir = "/tmp/OAI/"
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+log_path = os.path.join(log_dir, "stats.log")
+
+fh = logging.FileHandler(log_path)
+fh.setLevel(logging.ERROR)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+logger.addHandler(fh)
+logger.addHandler(ch)
 
 
 class OAIStats:
@@ -20,28 +46,36 @@ class OAIStats:
         self.statistics = {}
 
     # TODO: catch errors and save it into log file.
-    def run(self):
+    def run(self, stat_dir="/tmp/OAI/"):
         records = self.sickle.ListRecords(metadataPrefix=self.provider.metadata_prefix)
         for num, record in enumerate(records):
-            print(f"{num}.", "Record:", record.header.identifier)
-            for address in record.record_map.keys():
-                address_list = address.split(".")
-                address_queue = deque(address_list)
-                source = jmespath.search(sanitize_address(address), self.statistics)
-                if source is None:
-                    self.insert_value(address_queue, {}, self.statistics)
-                else:
-                    if address in record.field_map:
-                        fields = record.get_field(address)
-                        address = add_node(address, "field")
-                        address_list = address.split(".")
-                        source = jmespath.search(sanitize_address(address),
-                                                 self.statistics)
-                        if source is None:
-                            self.insert_value(deque(address_list), set(), self.statistics)
-                        else:
-                            self.statistics = update_node(address, self.statistics, fields)
-            print(self.statistics)
+            try:
+                logger.info(f"{num}. Record: {record.header.identifier}")
+                for address in record.record_map.keys():
+                    address_list = address.split(".")
+                    address_queue = deque(address_list)
+                    source = jmespath.search(sanitize_address(address), self.statistics)
+                    if source is None:
+                        self.insert_value(address_queue, {}, self.statistics)
+                    else:
+                        if address in record.field_map:
+                            fields = record.get_field(address)
+                            address = add_node(address, "field")
+                            address_list = address.split(".")
+                            source = jmespath.search(sanitize_address(address),
+                                                     self.statistics)
+                            if source is None:
+                                self.insert_value(deque(address_list), set(), self.statistics)
+                            else:
+                                self.statistics = update_node(address, self.statistics, fields)
+            except Exception as e:
+                logger.error(str(e))
+            finally:
+                if not os.path.exists(stat_dir):
+                    os.makedirs(stat_dir)
+                stat_path = os.path.join(log_dir, "stats.json")
+                with open(stat_path, "w") as fp:
+                    json.dump(self.statistics, fp, ensure_ascii=False, indent=4, cls=SetEncoder)
 
     def insert_value(self, addres_queue, value, node):
         """
