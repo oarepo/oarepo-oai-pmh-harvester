7c4630b8f19849ba54c68919da9ada8672071e4d
Daniel Kopecky <kopecky.d@gmail.com>
2020-05-15 13:43:21 +0200

    Removed old dependecies
    Rest due to file moves

--- example/rules/uk/doctype.py
+++ example/rules/uk/doctype.py
@@ -1,5 +1,5 @@
+from example.rules.utils import get_ref
 from invenio_oarepo_oai_pmh_harvester.register import Decorators
-from invenio_oarepo_oai_pmh_harvester.rules.utils import get_ref
 from invenio_oarepo_oai_pmh_harvester.transformer import OAITransformer
 
 
--- example/rules/uk/title.py
+++ example/rules/uk/title.py
@@ -1,5 +1,5 @@
+from example.rules.utils import get_iso_lang_code
 from invenio_oarepo_oai_pmh_harvester.register import Decorators
-from invenio_oarepo_oai_pmh_harvester.rules.utils import get_iso_lang_code
 from invenio_oarepo_oai_pmh_harvester.transformer import OAITransformer
 
 
