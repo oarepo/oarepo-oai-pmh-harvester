eb6c150de61c8f1b51601e9c32236a4605e6a381
Daniel Kopecký <kopecky.d@gmail.com>
2020-10-20 13:33:39 +0200

    Improved tests

--- tests/test_synchronization.py
+++ tests/test_synchronization.py
@@ -1,169 +1,150 @@
+from collections import defaultdict
 from datetime import datetime
+from itertools import islice
+from pprint import pprint
 
 import pytest
+import requests
 from invenio_records import Record
+from lxml.etree import _Element
+from pytest import skip
+from sickle.iterator import OAIItemIterator
 from sqlalchemy.orm.exc import NoResultFound
 
-from oarepo_oai_pmh_harvester.ext import OArepoOAIClient
-from oarepo_oai_pmh_harvester.models import OAIRecord, OAISync
+from oarepo_oai_pmh_harvester.models import OAIRecord, OAISync, OAIProvider
 from oarepo_oai_pmh_harvester.proxies import current_oai_client
 
 
-def test_get_endpoint_config(app, db, metadata):
-    synchronizer = current_oai_client.synchronizers["uk"]
-    res = synchronizer.get_endpoint_config(metadata)
-    assert res is not None
-    assert isinstance(res, dict)
+class TestSynchronization:
+    def test_get_endpoint_config(self, load_entry_points, app, db, metadata):
+        synchronizer = current_oai_client.synchronizers["uk"]
+        res = synchronizer.get_endpoint_config(metadata)
+        assert res is not None
+        assert isinstance(res, dict)
 
+    def test_create_record(self, load_entry_points, app, db, metadata, ):
+        synchronizers = app.extensions['oarepo-oai-client'].synchronizers
+        synchronizer = synchronizers["uk"]
+        rec = synchronizer.create_record(metadata)
+        assert rec == {'title': 'Testovací záznam', 'pid': '1'}
 
-def test_create_record(app, db, metadata):
-    synchronizers = current_oai_client.synchronizers
-    synchronizer = synchronizers["uk"]
-    rec = synchronizer.create_record(metadata)
-    assert rec == {'title': 'Testovací záznam', 'pid': '1'}
+    def test_update_record(self, load_entry_points, app, db, metadata):
+        synchronizers = current_oai_client.synchronizers
+        synchronizer = synchronizers["uk"]
+        record = synchronizer.create_record(metadata)
+        provider = OAIProvider.query.filter_by(code="uk").one_or_none()
+        if not provider:
+            current_oai_client.create_providers()
+        oai_sync = OAISync(provider_id=1)
+        db.session.add(oai_sync)
+        db.session.commit()
+        oai_rec = OAIRecord(
+            id=record.id,
+            oai_identifier="oai:example.cz:1",
+            creation_sync_id=oai_sync.id,
+            pid="1",
+            timestamp=datetime.now()
+        )
+        db.session.add(oai_rec)
+        db.session.commit()
+        metadata["title"] = "Updated record"
+        record2 = synchronizer.update_record(oai_rec, metadata)
+        assert record2 == {'title': 'Updated record', 'pid': '1'}
 
+    def test_delete_record(self, load_entry_points, app, db, metadata):
+        synchronizers = current_oai_client.synchronizers
+        synchronizer = synchronizers["uk"]
+        provider = OAIProvider.query.filter_by(code="uk").one_or_none()
+        if not provider:
+            current_oai_client.create_providers()
+        record = synchronizer.create_record(metadata)
+        oai_sync = OAISync(provider_id=1)
+        db.session.add(oai_sync)
+        db.session.commit()
+        oai_rec = OAIRecord(
+            id=record.id,
+            oai_identifier="oai:example.cz:1",
+            creation_sync_id=oai_sync.id,
+            pid="1",
+            timestamp=datetime.now()
+        )
+        db.session.add(oai_rec)
+        db.session.commit()
+        synchronizer.delete_record(oai_rec)
+        with pytest.raises(NoResultFound):
+            Record.get_record(oai_rec.id)
+        deleted_record = Record.get_record(oai_rec.id, with_deleted=True)
 
-def test_update_record(app, db, metadata):
-    synchronizers = current_oai_client.synchronizers
-    synchronizer = synchronizers["uk"]
-    record = synchronizer.create_record(metadata)
-    oai_sync = OAISync(provider_id=1)
-    db.session.add(oai_sync)
-    db.session.commit()
-    oai_rec = OAIRecord(
-        id=record.id,
-        oai_identifier="oai:example.cz:1",
-        creation_sync_id=oai_sync.id,
-        pid="1",
-        timestamp=datetime.now()
-    )
-    db.session.add(oai_rec)
-    db.session.commit()
-    metadata["title"] = "Updated record"
-    record2 = synchronizer.update_record(oai_rec, metadata)
-    assert record2 == {'title': 'Updated record', 'pid': '1'}
+    def test_delete(self, load_entry_points, app, db, metadata):
+        synchronizers = current_oai_client.synchronizers
+        synchronizer = synchronizers["uk"]
+        provider = OAIProvider.query.filter_by(code="uk").one_or_none()
+        if not provider:
+            current_oai_client.create_providers()
+        record = synchronizer.create_record(metadata)
+        oai_sync = OAISync(provider_id=1)
+        db.session.add(oai_sync)
+        db.session.commit()
+        oai_rec = OAIRecord(
+            id=record.id,
+            oai_identifier="oai:example.cz:1",
+            creation_sync_id=oai_sync.id,
+            pid="1",
+            timestamp=datetime.now()
+        )
+        db.session.add(oai_rec)
+        db.session.commit()
+        metadata["title"] = "Updated record"
+        synchronizer._delete(oai_rec)
+        with pytest.raises(NoResultFound):
+            Record.get_record(oai_rec.id)
+        deleted_record = Record.get_record(oai_rec.id, with_deleted=True)
 
+    def test_get_xml(self, load_entry_points, app, db):
+        synchronizers = current_oai_client.synchronizers
+        synchronizer = synchronizers["uk"]
+        try:
+            xml = synchronizer.get_xml("oai:dspace.cuni.cz:20.500.11956/111006")
+        except requests.exceptions.RequestException:
+            skip("Connection failed")
+        assert isinstance(xml, _Element)
 
-def test_delete_record(app, db, metadata):
-    synchronizers = current_oai_client.synchronizers
-    synchronizer = synchronizers["uk"]
-    record = synchronizer.create_record(metadata)
-    oai_sync = OAISync(provider_id=1)
-    db.session.add(oai_sync)
-    db.session.commit()
-    oai_rec = OAIRecord(
-        id=record.id,
-        oai_identifier="oai:example.cz:1",
-        creation_sync_id=oai_sync.id,
-        pid="1",
-        timestamp=datetime.now()
-    )
-    db.session.add(oai_rec)
-    db.session.commit()
-    metadata["title"] = "Updated record"
-    synchronizer.delete_record(oai_rec)
-    with pytest.raises(NoResultFound):
-        Record.get_record(oai_rec.id)
-    deleted_record = Record.get_record(oai_rec.id, with_deleted=True)
-    print(deleted_record)
+    def test_parse(self, load_entry_points, app, db, record_xml, parsed_record_xml):
+        def default_to_regular(d):
+            if isinstance(d, defaultdict):
+                d = {k: default_to_regular(v) for k, v in d.items()}
+            if isinstance(d, (list, tuple)):
+                d = [default_to_regular(_) for _ in d]
+            return d
 
-# import random
-# from datetime import datetime
-# from pprint import pprint
-# from unittest.mock import MagicMock
-#
-# import pytest
-# from requests import ConnectionError
-# from sickle import Sickle
-# from sickle.iterator import OAIItemIterator
-#
-# from oarepo_oai_pmh_harvester.exceptions import HandlerNotFoundError
-# from oarepo_oai_pmh_harvester.models import OAISync
-# from oarepo_oai_pmh_harvester.synchronization import OAISynchronizer
-#
-#
-# def test_init(app, test_db, migrate_provider):
-#     synchronizer = OAISynchronizer(migrate_provider)
-#     assert synchronizer is not None
-#
-#
-# def test_delete(migrate_provider):
-#     def delete_handler(oai_identifier):
-#         pass
-#
-#     synchronizer = OAISynchronizer(migrate_provider, delete_record=delete_handler)
-#     synchronizer.delete("bla")
-#
-#
-# def test_delete_2(migrate_provider):
-#     synchronizer = OAISynchronizer(migrate_provider)
-#     with pytest.raises(HandlerNotFoundError):
-#         synchronizer.delete("bla")
-#
-#
-# def test_synchronize_delete(migrate_provider):
-#     def delete_handler(oai_identifier, datestamp):
-#         print("deleted")
-#
-#     class Identifier:
-#         datestamp = datetime.utcnow()
-#         identifier = random.randint(0, 100000)
-#         deleted = True
-#
-#     identifiers = [Identifier() for x in range(10)]
-#     synchronizer = OAISynchronizer(migrate_provider, delete_record=delete_handler)
-#     synchronizer.synchronize(identifiers=identifiers)
-#     assert synchronizer.deleted == 10
-#
-#
-# def test_synchronize_create(migrate_provider, record_xml, sample_record):
-#     class Record:
-#         xml = record_xml
-#
-#     def create_handler(*args, **kwargs):
-#         return sample_record
-#
-#     @Decorators.parser("test_parser", "nusl")
-#     def parser(*args, **kwargs):
-#         return {
-#             "id": "1",
-#             "title": "Some title",
-#             "language": ["cze", "eng"]
-#         }
-#
-#     synchronizer = OAISynchronizer(migrate_provider,
-#                                    parser_name="test_parser",
-#                                    create_record=create_handler,
-#                                    id_handler=nusl_theses.attach_id
-#                                    )
-#     synchronizer.sickle.GetRecord = MagicMock(return_value=Record())
-#     synchronizer.transformer.transform = MagicMock(return_value={
-#         "id": "1",
-#         "title": "Some title",
-#         "language": ["cze", "eng"]
-#     })
-#     synchronizer.oai_sync = OAISync(provider_id=migrate_provider.id)
-#     record = synchronizer.create_or_update("bla", datetime.utcnow())
-#     assert record == {'id': '1', 'identifier': [{'value': 'oai:server:id', 'type':
-#     'originalOAI'}]}
-#
-#
-# def test_get_oai_identifiers(migrate_provider):
-#     oai_sync = OAISynchronizer(migrate_provider)
-#     try:
-#         results = oai_sync._get_oai_identifiers()
-#         assert isinstance(results, OAIItemIterator)
-#     except ConnectionError:
-#         pytest.skip("Connection error")
-#
-#
-# def test_get_oai_identifiers_2(migrate_provider):
-#     oai_sync = OAISynchronizer(migrate_provider)
-#     try:
-#         results = oai_sync._get_oai_identifiers(
-#             sickle=Sickle("https://invenio.nusl.cz/oai2d/"),
-#             metadata_prefix="marcxml",
-#         )
-#         assert isinstance(results, OAIItemIterator)
-#     except ConnectionError:
-#         pytest.skip("Connection error")
+        synchronizers = current_oai_client.synchronizers
+        synchronizer = synchronizers["uk"]
+        parsed = synchronizer.parse(record_xml)
+        res = default_to_regular(parsed)
+        assert res == parsed_record_xml
+
+    def test_get_oai_identifiers(self, load_entry_points, app, db):
+        synchronizers = current_oai_client.synchronizers
+        synchronizer = synchronizers["uk"]
+        ids = synchronizer._get_oai_identifiers()
+        assert isinstance(ids, OAIItemIterator)
+
+    def test_get_oai_identifiers_2(self, load_entry_points, app, db):
+        synchronizers = current_oai_client.synchronizers
+        synchronizer = synchronizers["uk"]
+        ids = synchronizer._get_oai_identifiers(
+            identifiers_list=["oai:dspace.cuni.cz:20.500.11956/111006"])
+        assert len(ids) == 1
+        assert isinstance(ids, list)
+
+    def test_get_identifiers(self, load_entry_points, app, db):
+        synchronizers = current_oai_client.synchronizers
+        synchronizer = synchronizers["uk"]
+        ids = synchronizer._get_identifiers()
+        assert isinstance(ids, islice)
+
+    def test_transform(self, load_entry_points, app, db):
+        synchronizers = current_oai_client.synchronizers
+        synchronizer = synchronizers["uk"]
+        transformed = synchronizer.transform({})
+        assert transformed == {}
--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -3,110 +3,106 @@ import pytest
 from oarepo_oai_pmh_harvester.transformer import OAITransformer
 
 
-def test_init():
-    transformer = OAITransformer(rules={}, unhandled_paths=set("/path/to/field"))
-    assert transformer.rules == {}
-    assert transformer.unhandled_paths == set("/path/to/field")
+class TestTransformer:
+    def test_init(self):
+        transformer = OAITransformer(rules={}, unhandled_paths=set("/path/to/field"))
+        assert transformer.rules == {}
+        assert transformer.unhandled_paths == set("/path/to/field")
 
+    def test_init_2(self):
+        transformer = OAITransformer()
+        assert transformer.rules == {}
+        assert transformer.unhandled_paths == set()
 
-def test_init_2():
-    transformer = OAITransformer()
-    assert transformer.rules == {}
-    assert transformer.unhandled_paths == set()
+    def test_iter_json_1(self):
+        def transform_handler(el, **kwargs):
+            return {"spam": el}
 
-
-def test_iter_json_1():
-    def transform_handler(el, **kwargs):
-        return {"spam": el}
-
-    record = {
-        "path": {
-            "to": {
-                "field": "bla"
-            }
-        },
-        "spam": "ham"
-    }
-    rules = {
-        "/spam": {
-            "pre": transform_handler
-        }
-    }
-    transformer = OAITransformer(rules=rules, unhandled_paths={"/path/to/field", })
-    result = transformer.transform(record)
-    assert result == {"spam": "ham"}
-
-
-def test_iter_json_2():
-    def transform_handler(el, **kwargs):
-        return {"spam": el}
-
-    record = {
-        "path": {
-            "to": {
-                "field": "bla"
-            }
-        },
-        "spam": ["ham", "blabla"]
-    }
-    rules = {
-        "/spam": {
-            "pre": transform_handler
-        }
-    }
-    transformer = OAITransformer(rules=rules, unhandled_paths={"/path/to/field", })
-    result = transformer.transform(record)
-    assert result == {"spam": ["ham", "blabla"]}
-
-
-def test_iter_json_3():
-    def transform_handler(paths, el, results, phase, **kwargs):
-        results[0]["spam"] = el
-        return OAITransformer.PROCESSED
-
-    record = {
-        "path": {
-            "to": {
-                "field": "bla"
-            }
-        },
-        "spam": "ham"
-    }
-    rules = {
-        "/spam": {
-            "pre": transform_handler
-        }
-    }
-    transformer = OAITransformer(rules=rules)
-    with pytest.raises(ValueError):
-        transformer.transform(record)
-
-
-def test_iter_json_4():
-    def transform_handler(paths, el, results, phase, **kwargs):
-        results[0]["spam"] = el
-        return OAITransformer.PROCESSED
-
-    record = {
-        "path": {
-            "to": {
-                "field": "bla"
-            }
-        },
-        "spam": [
-            {
-                "ham": "blabla",
+        record = {
+            "path": {
+                "to": {
+                    "field": "bla"
+                }
             },
-            {
-                "unhandled_option": ["nothing"]
-            }
-        ]
-    }
-    rules = {
-        "/spam/ham": {
-            "pre": transform_handler
+            "spam": "ham"
         }
-    }
-    transformer = OAITransformer(rules=rules, unhandled_paths={"/path/to/field", })
-    with pytest.raises(ValueError):
+        rules = {
+            "/spam": {
+                "pre": transform_handler
+            }
+        }
+        transformer = OAITransformer(rules=rules, unhandled_paths={"/path/to/field", })
         result = transformer.transform(record)
+        assert result == {"spam": "ham"}
+
+    def test_iter_json_2(self):
+        def transform_handler(el, **kwargs):
+            return {"spam": el}
+
+        record = {
+            "path": {
+                "to": {
+                    "field": "bla"
+                }
+            },
+            "spam": ["ham", "blabla"]
+        }
+        rules = {
+            "/spam": {
+                "pre": transform_handler
+            }
+        }
+        transformer = OAITransformer(rules=rules, unhandled_paths={"/path/to/field", })
+        result = transformer.transform(record)
+        assert result == {"spam": ["ham", "blabla"]}
+
+    def test_iter_json_3(self):
+        def transform_handler(paths, el, results, phase, **kwargs):
+            results[0]["spam"] = el
+            return OAITransformer.PROCESSED
+
+        record = {
+            "path": {
+                "to": {
+                    "field": "bla"
+                }
+            },
+            "spam": "ham"
+        }
+        rules = {
+            "/spam": {
+                "pre": transform_handler
+            }
+        }
+        transformer = OAITransformer(rules=rules)
+        with pytest.raises(ValueError):
+            transformer.transform(record)
+
+    def test_iter_json_4(self):
+        def transform_handler(paths, el, results, phase, **kwargs):
+            results[0]["spam"] = el
+            return OAITransformer.PROCESSED
+
+        record = {
+            "path": {
+                "to": {
+                    "field": "bla"
+                }
+            },
+            "spam": [
+                {
+                    "ham": "blabla",
+                },
+                {
+                    "unhandled_option": ["nothing"]
+                }
+            ]
+        }
+        rules = {
+            "/spam/ham": {
+                "pre": transform_handler
+            }
+        }
+        transformer = OAITransformer(rules=rules, unhandled_paths={"/path/to/field", })
+        with pytest.raises(ValueError):
+            result = transformer.transform(record)
