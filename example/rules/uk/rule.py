10ac454e19aa29d2df75d83dc4b48a922e97ad65
Daniel Kopecký <kopecky.d@gmail.com>
2020-10-21 13:49:32 +0200

    Impelemted rule

--- example/rules/uk/rule.py
+++ example/rules/uk/rule.py
@@ -1,6 +1,7 @@
 from oarepo_oai_pmh_harvester.proxies import current_oai_client
 
 
-@current_oai_client.rule("/test/path", "xoai")
-def rule():
-    return "ahoj"
+@current_oai_client.rule("/dc/title", "xoai")
+def rule(el, **kwargs):
+    value_ = el[0]["value"][0]
+    return {"title": value_}
