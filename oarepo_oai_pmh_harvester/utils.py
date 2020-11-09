faed346a58c7cee331592d71437990440d043c65
Daniel Kopecký <kopecky.d@gmail.com>
2020-11-09 17:48:07 +0100

    Rules are merged (included nested dicts) instead only updated

--- oarepo_oai_pmh_harvester/utils.py
+++ oarepo_oai_pmh_harvester/utils.py
@@ -20,3 +20,19 @@ def get_oai_header_data(header: Header = None, xml: _Element = None):
     oai_identifier = header.identifier
     deleted = header.deleted
     return datestamp, deleted, oai_identifier
+
+
+def merge(a, b, path=None):
+    "merges b into a"
+    if path is None: path = []
+    for key in b:
+        if key in a:
+            if isinstance(a[key], dict) and isinstance(b[key], dict):
+                merge(a[key], b[key], path + [str(key)])
+            elif a[key] == b[key]:
+                pass  # same leaf value
+            else:
+                raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
+        else:
+            a[key] = b[key]
+    return a
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
+import pytest
 from sickle.models import Header
 
-from oarepo_oai_pmh_harvester.utils import get_oai_header_data
+from oarepo_oai_pmh_harvester.utils import get_oai_header_data, merge
 
 
 def test_get_oai_header_data(load_entry_points, app, db, record_xml):
@@ -13,3 +14,10 @@ def test_get_oai_header_data(load_entry_points, app, db, record_xml):
 def test_get_oai_header_data_2(load_entry_points, app, db, record_xml):
     res_tuple = get_oai_header_data(xml=record_xml)
     assert res_tuple == ('2017-09-11T08:12:53Z', False, 'oai:dspace.cuni.cz:20.500.11956/2623')
+
+
+def test_merge():
+    res = merge({1: {"a": "A"}, 2: {"b": "B"}}, {2: {"c": "C"}, 3: {"d": "D"}})
+    with pytest.raises(Exception):
+        merge({1: {"a": "A"}, 2: {"b": "B"}}, {1: {"a": "A"}, 2: {"b": "C"}})
+    assert res == {1: {'a': 'A'}, 2: {'b': 'B', 'c': 'C'}, 3: {'d': 'D'}}
