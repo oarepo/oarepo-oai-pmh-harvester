9b71493f19fe8e295c2c6655fddf41da3c113a15
Daniel Kopecký <kopecky.d@gmail.com>
2021-01-27 10:02:22 +0100

    Change signature of pre_processor decorator

--- oarepo_oai_pmh_harvester/decorators.py
+++ oarepo_oai_pmh_harvester/decorators.py
@@ -1,3 +1,5 @@
+from typing import List, Dict
+
 from oarepo_oai_pmh_harvester.proxies import current_oai_client
 from oarepo_oai_pmh_harvester.transformer import OAITransformer
 
@@ -16,22 +18,46 @@ def parser(name):
     return wrapper
 
 
-def endpoint_handler(provider, parser):
+def endpoint_handler(provider: str = None, parser: str = None,
+                     provider_parser_list: List[Dict] = None):
+    if provider_parser_list and (provider or parser):
+        raise Exception(
+            "If the provider and parser is specified, cannot specify provider_parser_list")
+    if (provider and not parser) or (parser and not provider):
+        raise Exception("Must specify provider and parser")
+
     def wrapper(func):
-        current_oai_client.add_endpoint_handler(func, provider, parser)
+        current_oai_client.add_endpoint_handler(func, provider=provider, parser_name=parser,
+                                                provider_parser_list=provider_parser_list)
 
     return wrapper
 
 
-def pre_processor(provider, parser):
+def pre_processor(provider: str = None, parser: str = None,
+                  provider_parser_list: List[Dict] = None):
+    if provider_parser_list and (provider or parser):
+        raise Exception(
+            "If the provider and parser is specified, cannot specify provider_parser_list")
+    if (provider and not parser) or (parser and not provider):
+        raise Exception("Must specify provider and parser")
+
     def wrapper(func):
-        current_oai_client.add_pre_processor(func, provider, parser)
+        current_oai_client.add_pre_processor(func, provider=provider, parser_name=parser,
+                                             provider_parser_list=provider_parser_list)
 
     return wrapper
 
 
-def post_processor(provider, parser):
+def post_processor(provider: str = None, parser: str = None,
+                   provider_parser_list: List[Dict] = None):
+    if provider_parser_list and (provider or parser):
+        raise Exception(
+            "If the provider and parser is specified, cannot specify provider_parser_list")
+    if (provider and not parser) or (parser and not provider):
+        raise Exception("Must specify provider and parser")
+
     def wrapper(func):
-        current_oai_client.add_post_processor(func, provider, parser)
+        current_oai_client.add_post_processor(func, provider=provider, parser_name=parser,
+                                              provider_parser_list=provider_parser_list)
 
     return wrapper
