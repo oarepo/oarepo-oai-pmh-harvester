02f860c656ca56fff29af33bc9c9ca4673372fe3
Daniel Kopecký <kopecky.d@gmail.com>
2021-01-28 10:32:14 +0100

    Changed tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -17,7 +17,10 @@ def test_run_2(load_entry_points, app, db):
     patch = mock.patch('sickle.app.Sickle.harvest', mock_harvest)
     patch.start()
     runner = app.test_cli_runner()
-    result = runner.invoke(run, ["-p", "uk", "-s", "xoai", "-a", "oai:test.example.com:1996652"])
+    result = runner.invoke(run, ["-p", "uk", "-s", "xoai", "-a", "oai:test.example.com:1585322"])
+    assert result.exit_code == 0
+    result = runner.invoke(run, ["-p", "uk", "-s", "xoai", "-a", "oai:test.example.com:1585322",
+                                 "--overwrite"])
     assert result.exit_code == 0
     patch.stop()
 
