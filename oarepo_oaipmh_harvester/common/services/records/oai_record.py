16588c5071dfd9d6ab2a31b3fa9b0fe82d78d532
Alzpetka <lili3.pokorna@seznam.cz>
2024-07-15 13:25:50 +0200

    error message

--- oarepo_oaipmh_harvester/common/services/records/oai_record.py
+++ oarepo_oaipmh_harvester/common/services/records/oai_record.py
@@ -7,7 +7,9 @@ class BaseOaiRecordSchema(BaseRecordSchema):
     def process_transformers(self, data, **kwargs):
         error_str = ""
         for e in data["errors"]:
-            error_str = error_str + f'{e["location"]}: {e["message"]}, '
-
+            if "info" in e:
+                error_str = error_str + f'{e["location"]}: {e["message"]} - {e["info"]}, '
+            else:
+                error_str = error_str + f'{e["location"]}: {e["message"]}, '
         data["errors"] = error_str
         return data
