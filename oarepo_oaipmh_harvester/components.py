bc2901dd48c40f9a609f5c01182ebb6c23a8737a
ducica <dusanstojanovic256@gmail.com>
2024-10-21 08:35:23 +0200

    conditionally getting oai key from record

--- oarepo_oaipmh_harvester/components.py
+++ oarepo_oaipmh_harvester/components.py
@@ -1,11 +1,12 @@
 from invenio_records_resources.services.records.components import ServiceComponent
 
+
 class OaiSectionComponent(ServiceComponent):
     def create(self, identity, data=None, record=None, **kwargs):
-        record["oai"] = data["oai"]
-        
+        record["oai"] = data.get("oai", {})
+
     def update(self, identity, data=None, record=None, **kwargs):
-        record["oai"] = data["oai"]
+        record["oai"] = data.get("oai", {})
 
     def publish(self, identity, draft=None, record=None, **kwargs):
         record["oai"] = draft.get("oai", {})
