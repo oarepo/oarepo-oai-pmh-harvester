39e73cf7b9033cbd8299eba5ff8d047d993288fc
Mirek Simek <miroslav.simek@gmail.com>
2025-04-03 10:32:53 +0200

    type: added typings

--- oarepo_oaipmh_harvester/config.py
+++ oarepo_oaipmh_harvester/config.py
@@ -1,4 +1,7 @@
+from typing import Any
+
 from invenio_i18n import lazy_gettext as _
+from oarepo_runtime.datastreams import BaseReader, BaseTransformer, BaseWriter
 
 from oarepo_oaipmh_harvester.permissions import OAIHarvesterPermissions
 from oarepo_oaipmh_harvester.readers.oai_dir import OAIDirReader
@@ -10,66 +13,73 @@ from oarepo_oaipmh_harvester.transformers.record_lookup import (
 from oarepo_oaipmh_harvester.writers.oai import OAIWriter
 from oarepo_oaipmh_harvester.writers.oai_dir import OAIDirWriter
 
-DATASTREAMS_READERS = {"sickle": SickleReader, "oai_dir": OAIDirReader}
+DATASTREAMS_READERS: dict[str, type[BaseReader]] = {
+    "sickle": SickleReader,
+    "oai_dir": OAIDirReader,
+}
 
-DATASTREAMS_TRANSFORMERS = {
+DATASTREAMS_TRANSFORMERS: dict[str, type[BaseTransformer]] = {
     "marcxml": MarcXMLTransformer,
     "oai_record_lookup": OAIRecordLookupTransformer,
 }
 
-DATASTREAMS_WRITERS = {"oai_dir": OAIDirWriter, "oai": OAIWriter}
+DATASTREAMS_WRITERS: dict[str, type[BaseWriter]] = {
+    "oai_dir": OAIDirWriter,
+    "oai": OAIWriter,
+}
 
 OAREPO_PERMISSIONS_PRESETS = {"oai_harvester": OAIHarvesterPermissions}
 
 
-OAI_RUN_SEARCH = {
+OAI_RUN_SEARCH: dict[str, Any] = {
     "facets": [],
     "sort": ["newest"],
     "sort_default": "newest",
     "sort_default_no_query": "newest",
 }
-OAI_RUN_SORT_OPTIONS = {
+OAI_RUN_SORT_OPTIONS: dict[str, Any] = {
     "newest": dict(
         title=_("Newest"),
         fields=["-created"],
     ),
 }
 
-OAI_BATCH_SEARCH = {
+OAI_BATCH_SEARCH: dict[str, Any] = {
     "facets": [],
     "sort": ["newest"],
     "sort_default": "newest",
     "sort_default_no_query": "newest",
 }
 
-OAI_BATCH_SORT_OPTIONS = {
+OAI_BATCH_SORT_OPTIONS: dict[str, Any] = {
     "newest": dict(
         title=_("Newest"),
         fields=["-created"],
     ),
 }
 
-OAI_HARVESTER_SORT_OPTIONS = {
+OAI_HARVESTER_SORT_OPTIONS: dict[str, Any] = {
     "newest": dict(
         title=_("Newest"),
         fields=["-created"],
     ),
 }
 
-OAI_HARVESTER_SEARCH = {
+OAI_HARVESTER_SEARCH: dict[str, Any] = {
     "facets": [],
     "sort": ["newest"],
     "sort_default": "newest",
     "sort_default_no_query": "newest",
 }
 
-OAI_RECORD_SEARCH = {
+OAI_RECORD_SEARCH: dict[str, Any] = {
     "facets": [],
     "sort": ["newest"],
     "sort_default": "newest",
     "sort_default_no_query": "newest",
 }
-OAI_RECORD_SORT_OPTIONS = {
+
+OAI_RECORD_SORT_OPTIONS: dict[str, Any] = {
     "newest": dict(
         title=_("Newest"),
         fields=["-created"],
